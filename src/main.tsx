7aa337bb5c3268ee3214e5a041ed9814ed47f6c4
Reggie <speedracr35@gmail.com>
2025-06-10 12:14:24 +0800

    Cards Back

--- src/main.tsx
+++ src/main.tsx
@@ -17,9 +17,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path="/about" element={<AboutUsPage />} />
         <Route path="/app/cards" element={<CardHubExperiment />} />
         <Route path="/app/jobs" element={<SwipeApp onCollapse={() => {}} userType={null} jobListings={[
-          {company: 'Microsoft', title: 'Software Engineer', location: 'Remote', description: 'Build cool stuff!'},
-          {company: 'Google', title: 'Frontend Developer', location: 'NYC', description: 'Work on search UI.'},
-          {company: 'Amazon', title: 'Cloud Architect', location: 'Seattle', description: 'Design AWS solutions.'}
+          {company: 'Microsoft', title: 'Software Engineer', location: 'Remote', description: 'Build cool stuff!', logo: 'https://pngimg.com/d/microsoft_PNG13.png'},
+          {company: 'Google', title: 'Frontend Developer', location: 'NYC', description: 'Work on search UI.', logo: 'https://pngimg.com/d/google_PNG19644.png'},
+          {company: 'Amazon', title: 'Cloud Architect', location: 'Seattle', description: 'Design AWS solutions.', logo: 'https://pngimg.com/d/amazon_PNG11.png'}
         ]} />} />
       </Routes>
     </BrowserRouter>
